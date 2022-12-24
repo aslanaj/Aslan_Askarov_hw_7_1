9dcdffc952852311febd4ea49d64a7d227ccf76b
Aslan AJ <theajaslan@gmail.com>
2022-12-25 02:46:25 +0600

    Initial commit

---
+++ src/Medic.java
@@ -0,0 +1,10 @@
+public class Medic extends Hero{
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Medic применил суперспособность CRITICAL DAMAGE");
+    }
+    private int healPoints;
+    public void increaseExperience(){
+
+    }
+}
